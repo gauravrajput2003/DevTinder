fb34fd3bdd69916ef6da8b80d091a21f16cab329
Your Name <gauravkumarsingh010103@gmail.com>
2025-04-18 10:33:19 +0530

    mongodb connected

--- src/app.js
+++ src/app.js
@@ -1,48 +1,25 @@
-
 const express = require("express");
+const connDB = require("./config/database"); // 
 const app = express();
-// app.get("/user", (req, res) => {
-//     res.send({"name":"gauarv","last":"singh"});
-// });
-// app.delete("/user", (req, res) => {
-//     res.send("databse deleted successfully");
-// });
-// app.use("/test", (req, res) => {
-//     res.send("hello from abes test");
-// });
-// app.post("/profile",(req,res)=>{
-// res.send("profile updated success");
-// })
-// app.use("/bihar", (req, res) => {
-//     res.send("hello from abes bihar");
-// });
-// app.use("/test", (req, res) => {
-//     res.send("hello from abes test");
-// });
-// app.use("/", (req, res,next) => {
-//     res.send("hello from abes");
-//     next();
-// });
-// app.get("/user",
-//      (req, res,next) => {
-//     res.send({"Router 1st is calling over here"});
-//     next();
-// },
-//      (req, res,next) => {
-//     res.send({"Router 2nd is calling over here "});next();
-// },
-// );           
-//handle auth middleware for only get requests get,post
-const {adminAuth}=require("./middlewares/Auth");
-app.use("/admin",adminAuth);
-app.get("/admin/getAlloc",(req,res)=>{
 
-    res.send("all data send");
-})
-app.get("/admin/deleteUser",(req,res)=>{
+(async () => {
+    try {
+        await connDB(); 
+    } catch (err) {
+        console.error("Failed to connect to the database. Exiting...");
+        process.exit(1); 
+    }
+})();
+
+app.get("/admin/deleteUser", (req, res) => {
     res.send("Deleted a user");
-})
-// Start the server
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong");
+});
+
 app.listen(9931, () => {
     console.log("server running on 9931...");
-});
+});  
---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connDB = async () => {
+    try {
+        await mongoose.connect(
+            "mongodb+srv://project01:Animal%40%408000@project01.zfjbz.mongodb.net/?retryWrites=true&w=majority&appName=project01/devTinder "
+        );
+        console.log("Database connected successfully");
+    } catch (err) {
+        console.error("Database connection failed:", err);
+        throw err; 
+    }
+};
+
+module.exports = connDB;
