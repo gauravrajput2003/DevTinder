d1675b3f6aee1cfe51aa882ead0ed533c97ce1e1
Your Name <gauravkumarsingh010103@gmail.com>
2025-05-12 22:26:22 +0530

    added validator

--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,46 @@
 const mongoose = require("mongoose");
-
+const validator=require("validator" );
 const UserScheme = mongoose.Schema({
     firstName:{
     type:String,
     required:true,
     },
     lastName:{
-    type:String,
+    type:String, 
   
     },
     email:{
     type:String,
     lowercase:true,
     required:true,
-    unique:true
+    unique:true,
+    validate(value){
+        if(!validator.isEmail(value)){
+            throw new Error("email is invalid");
+        }
+    }
     },
     password:{
     type:String,
+      validate(value){
+        if(!validator.isStrongPassword(value)){
+            throw new Error("Enter a Strong Password");
+        }
+    }
    
     },
    
     age:{
     type:Number,
     min:18,
+    },
+    phone:{
+type:String,
+  validate(value){
+        if(!validator.isMobilePhone(value)){
+            throw new Error("phone is  invalid");
+        }
+    }
     },
    
     gender:{
@@ -36,6 +54,11 @@ const UserScheme = mongoose.Schema({
     photoUrl:{
         type:String,
         default:"https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fwww.gravatar.com%2Favatar%2F2c7d99fe281ecd3bcd65ab915bac6dd5%3Fs%3D250",
+          validate(value){
+        if(!validator.isURL(value)){
+            throw new Error("url  is invalid");
+        }
+    }
     },
     about:{
         type:String,
